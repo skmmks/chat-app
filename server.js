69ec39396101316e396a2b61152fc3df73f9c459
Steve Min <stevekmin@gmail.com>
2020-12-23 07:22:20 -0800

    add HTTP create server tohandle upcoming socket io

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
 //Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
