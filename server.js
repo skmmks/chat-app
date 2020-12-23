155d5aa2f7ef98373f2fc339f7c8499d9f5332db
Steve Min <stevekmin@gmail.com>
2020-12-23 07:00:13 -0800

    set static folder from server js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
