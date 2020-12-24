b0bfdd269edc33fcb78f7463358220afca4eb430
Steve Min <stevekmin@gmail.com>
2020-12-24 13:03:18 -0800

    add welcome message on socket login

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', (socket) => {
   console.log('New WS Connection....');
+
+  socket.emit('message', 'Welcome to ChatCord');
 });
 
 const PORT = process.env.PORT || 3000;
