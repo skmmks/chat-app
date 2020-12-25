97a56bc2220c3dea94ba9ea93355bc2d673ba187
Steve Min <stevekmin@gmail.com>
2020-12-25 07:51:01 -0800

    capture user input message to socket io

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,7 @@ socket.on('message', (message) => {
 
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+  socket.emit('chatMessage', msg);
 });
