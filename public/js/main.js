6a966758d8b942c295a6829359ae7d4dd48b1921
Steve Min <stevekmin@gmail.com>
2020-12-24 21:21:53 -0800

    add event listener to submit form

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', (message) => {
   console.log(message);
+});
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 });
