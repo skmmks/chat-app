b53a25ed9f953436532ed77b94742290f6826df6
Steve Min <stevekmin@gmail.com>
2020-12-28 08:03:19 -0800

    text box will scroll when new message is added

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 socket.on('message', (message) => {
   console.log(message);
   outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener('submit', (e) => {
