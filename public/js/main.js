52e8735d8b3a765f5256eb73dcbfdd3543c531e3
Steve Min <stevekmin@gmail.com>
2020-12-29 08:25:59 -0800

    destructure data to obtain username and room from query string

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
 const socket = io();
 
 socket.on('message', (message) => {
