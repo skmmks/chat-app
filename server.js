7665d02e597499f87d5ef803e58aab55c17fc5fd
Steve Min <stevekmin@gmail.com>
2020-12-31 07:18:27 -0800

    remove hard coded 'USER' name and add acutal user name from socket id

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ io.on('connection', (socket) => {
 
   // Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg));
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
   });
 
   //Broadcast when disconnect
